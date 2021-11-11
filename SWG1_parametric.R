7470f5727ae976875f5838bec2c565a840f5a574
Mandela Houngnibo <33883241+hmandela@users.noreply.github.com>
2021-11-11 12:01:40 +0100

    Some modifications to SWG1

--- SWG1_parametric.R
+++ SWG1_parametric.R
@@ -1,107 +1,52 @@
-two_parts_model=function(chemin.in,seas,prevu,n,distrubtion,nyear,andebut,ht_seuil){
-  #library
-  library(dplyr)
-  library(foreach)
-  library(readr)
-  library(doParallel)
-  library(parallel)
-  library(markovchain)
-  library(Rsolnp)
-  library(fitdistrplus)
-  library(vars)
-  library(lubridate)
-  library(matlib)
-  library(reshape2)
-  library(xts)
-  
-  
-  nb_cores <- detectCores() - 1
-  cl <- makeCluster(nb_cores)
-  registerDoParallel(cl)  
-  
+SWG1=function(chemin.in,yearclimcond,seas,distribution,nyear,andebut,ht_seuil){
+
   #read station files
   kandi_val<- read_csv(chemin.in, col_types = cols(year = col_character()))
   Clim_real=kandi_val
 
-####################Briggs and Wilks Methodology################## 
-  #Select season
-  saison_tampon=subset(Clim_real,mois%in%seas:(seas+2))
+  #####Model Vectorile AutoRegessif for other variables (TMAX,TMIN...)
   
-  #Compute seasonal cumulative rainfall
-  saison_tampon=saison_tampon %>%
-    transmute(year=year,PRCP=PRCP,season=rep(1,dim(saison_tampon)[1]))
-  saison_tampon=saison_tampon%>%group_by(year,season)%>%summarise(PRCP=sum(PRCP))
-  #cleaning df
-season=saison_tampon%>%dplyr::select(year=year,rain=PRCP)
-moy=mean(season$rain,na.rm = T)
-remove(saison_tampon)
-
-#compute terciles
-  tercile=quantile(season$rain,probs = seq(0,1,1/3))
-
-# Find years in each categorial
-
-  belowyear=as.vector(subset(season, 
-                             rain<tercile[2],select = year)$year)
-  aboveyear=as.vector(subset(season, 
-                             rain>tercile[3],select = year)$year)  
-  normalyear=as.vector(subset(season, 
-                      rain>tercile[2]&rain<tercile[3],select = year)$year) 
-
-  
-  
-  
-#Bootsrapping
-  sample_below=sample(belowyear,n*prevu[1],replace=T)
-  sample_normal=sample(normalyear,n*prevu[2],replace=T)
-  sample_above=sample(aboveyear,n*prevu[3],replace = T)
-  
-#Consitent climatology
-  yearclimcond=c(sample_below,sample_normal,sample_above)
- 
-
-   ##############Modele VAR pour les autres paramètres#######
-   
-  #Mise en forme zscore et série temporelle
+  #Standardization 
   no_rain=Clim_real[,-c(4,5)]
   
-  #log(max(rad)-rad) et sqrt(vent)
-  maxi_insol=max(no_rain$RAD)
-   
+  #Transform radiation and wind
+  #log(max(rad)-rad) et sqrt(wind)
+  maxi_insol=max(no_rain$RAD,na.rm = T)
+  
   rain_rain=Clim_real%>%transmute(year=year,month=month,day=day,PRCP=PRCP,TMAX=TMAX, TMIN=TMIN,UMAX=UMAX,UMIN=UMIN, RAD=log(maxi_insol-RAD),WIND=sqrt(WIND))
   
-
   no_rain=no_rain%>%transmute(year=year,month=month,day=day,TMAX=TMAX, TMIN=TMIN,UMAX=UMAX,UMIN=UMIN, RAD=log(maxi_insol-RAD),WIND=sqrt(WIND))
   
   no_rain_wide=reshape2::melt(data=no_rain, measure.vars=names(no_rain)[-c(1,2,3)], id.vars=c("year","month","day"))
   
   no_rain_wide <- dcast(no_rain_wide, month + day+variable ~ year)
+  
   t=no_rain_wide[,-c(1,2,3)]
-  zscore = foreach(i =1:dim(t)[1], .combine=rbind)%dopar%t(scale(as.numeric(t[i,])))
+  
+  zscore = foreach(i =1:dim(t)[1], .combine=rbind)%do%t(scale(as.numeric(t[i,])))
+  
   colnames(zscore)=names(t)
+  
   zscore=cbind(no_rain_wide[,1:3],zscore)
+  
   zscore=reshape2::melt(data=zscore, measure.vars=names(zscore)[-c(1,2,3)], id.vars=c("month","day","variable"))
   colnames(zscore)=c("month","day","elmnt","year","value")
   
-  ###tri
+  ###order and put in temporal series
   zscore=zscore[order( zscore[,3] ),]
   zscore<- dcast(zscore, year + month + day~ elmnt)
   zscore=zscore[complete.cases(zscore),]
   zscore=xts(zscore[,-c(1,2,3)],as.Date(paste(zscore$year,zscore$month,zscore$day, sep = "-")))
   
 
-  
-
-  
-  
- # fixer les longueurs des mois
+ # Set month length
  if(seas%in%c(3,5,8)){lmois=c(31,30,31)}
  if(seas%in%c(4,9)){lmois=c(30,31,30)}
  if(seas==6){lmois=c(30,31,31)}
  if(seas==7){lmois=c(31,31,30)}  
   
   
-  #calcul des Paramètres du générateurs
+  #Computing SWGs Parameters for rain and other variables (TMAX,TMIN...)
   jjseas=0
   cum_moy_seas_real=0
   trans_mat=list(0)
@@ -114,20 +59,20 @@ remove(saison_tampon)
   u_sigma1=list(0)
   moy_nonprcp=list(0)
   repeat{
-    #modèle var par mois pour les autres variables autres que PRCP
+#VAR model by month for non_rain variable 
 model.var=VAR(zscore[month(zscore)==as.numeric(seas+jjseas),],1, type = "none")
 coeftampon=Acoef(model.var)[[1]]
-#matrice A
+#matrix A
 coefA[[jjseas+1]]=coeftampon
 
 acftampon=acf(zscore[month(zscore)==as.numeric(seas+jjseas),],type = "correlation", plot = F,lag.max = 1)
 
-#Matrices Mo,M1 et des résidus(écart type)
+#Matrix Mo,M1 et des résidus
 coef0[[jjseas+1]]=acftampon$acf[1,,]
 coef1[[jjseas+1]]=acftampon$acf[2,,]
 coefS[[jjseas+1]]=coef0[[jjseas+1]]-coefA[[jjseas+1]]%*%t(coef1[[jjseas+1]])
 
-#moyennes et écart types des jours pluvieux/non pluvieux
+#mean and sd for rain/no rain days
 
 u_tampon1=colMeans(rain_rain[rain_rain$year%in%yearclimcond&as.numeric(rain_rain$month)==(jjseas+seas)&rain_rain$PRCP>=ht_seuil,-c(1,2,3,4)], na.rm = T)
 
@@ -147,16 +92,17 @@ u_sigma0[[jjseas+1]]= tampon_u_sig0
   
 u_sigma1[[jjseas+1]]= tampon_u_sig1
 
- #moyenne pour facteur
+ #mean for Bougthon factor possibility
 moy_nonprcp[[jjseas+1]]=colMeans(rain_rain[rain_rain$year%in%yearclimcond&as.numeric(rain_rain$month)==(jjseas+seas),-c(1,2,3,4)], na.rm = T)
 
- ### initialisation mois et année climato cohérente
-  A=matrix(nrow = 2, ncol = 2, 0)
+ ### Initialization month and year consistent with seasonal forecast prob mois et année climato cohérente
+ ### transition matrix for rain occurence  
+A=matrix(nrow = 2, ncol = 2, 0)
   cum_real=0
   
   for(i in yearclimcond){
-  #matrices de transition
-        month1=Clim_real[Clim_real$year==i&Clim_real$mois==(seas+jjseas),]$PRCP
+    #transition matrix
+    month1=Clim_real[Clim_real$year==i&Clim_real$mois==(seas+jjseas),]$PRCP
     cum_real=cum_real+sum(month1, na.rm = T)
     
     isec=which(month1<ht_seuil)
@@ -175,7 +121,7 @@ moy_nonprcp[[jjseas+1]]=colMeans(rain_rain[rain_rain$year%in%yearclimcond&as.num
   cum_moy_seas_real=cum_moy_seas_real+cum_real
   
   
-  #quantité de pluie
+  #rain amount
   month1_amount=NULL
   for(i in yearclimcond){
     monthh=filter(Clim_real,year==i,mois==(seas+jjseas))
@@ -183,13 +129,11 @@ moy_nonprcp[[jjseas+1]]=colMeans(rain_rain[rain_rain$year%in%yearclimcond&as.num
   }
   
   
-  #month1_amount= foreach(i =yearclimcond, .combine=c) %dopar%
-    #(subset(Clim_real,year=i,mois==(seas+jjseas),select =c(PRCP))$PRCP)
-  
+ 
   month1_amount=month1_amount[which(month1_amount>=ht_seuil)]
   
-  #Ajustement distribution des pluies journalières
-  if(distrubtion==1){
+  #Fitting daily rain amount 
+  if(distribution==1){
     amount_pargm <- fitdist(month1_amount, "exp")
     param=amount_pargm$estimate #paramètres du modèle
   }else{ 
@@ -205,23 +149,24 @@ moy_nonprcp[[jjseas+1]]=colMeans(rain_rain[rain_rain$year%in%yearclimcond&as.num
   }
   
 para_gene_pluie=list(matrices=trans_mat,para_qant=parametr,cum_real.clim=cum_moy_seas_real)
-  
-#initialisation toutes les années
+
+################Simulation###################################  
+#initialization for all years
  nbyear=0
  startyear=andebut
  prcpSimSeasonyear=NULL
  nonprcpSimSeasonyear=NULL
  
 repeat{ 
-  #initialisation annuel
+  #initialization 
   prcpSimSeason=NULL
   nonprcpSimseason=NULL
   jseas=0
   repeat{
-#initialisation autoragression
+#initialization autoregression
 Z0=mvrnorm(n = 1, rep(0,6), coef0[[jseas+1]])
 
-#proba inconditionelle des jours pluvieux
+#Inconditional probability of rainy days
 pi=trans_mat[[jseas+1]][2,1]/(1+trans_mat[[jseas+1]][2,1]-trans_mat[[jseas+1]][1,1])
 
  month11=NULL
@@ -234,7 +179,7 @@ nonprcpSim=(Z0*u_sigma0[[jseas+1]][,2]+u_sigma0[[jseas+1]][,1])
   }
  
    
- if(distrubtion==1){
+ if(distribution==1){
  month11=ifelse(u<=pi,qexp(runif(1,0,1),parametr[[jseas+1]]),0)
   }else{
  month11=ifelse(u<=pi,qgamma(runif(1,0,1),parametr[[jseas+1]][1],parametr[[jseas+1]][2]),0)
@@ -254,7 +199,7 @@ for(i in 1:ms){
   #precipitation variables
  
     #choix de la loi
-    if(distrubtion==1){
+    if(distribution==1){
 month11=c(month11,qexp(runif(1,0,1),parametr[[jseas+1]]))
     }else{
 month11=c(month11,qgamma(runif(1,0,1),parametr[[jseas+1]][1],parametr[[jseas+1]][2]))
@@ -269,7 +214,7 @@ nonprcpSim=cbind(nonprcpSim,(Z0*u_sigma1[[jseas+1]][,2]+u_sigma1[[jseas+1]][,1])
      #precipitation variables
       #choix de la loi
 
-      if(distrubtion==1){
+      if(distribution==1){
         month11=c(month11,qexp(runif(1,0,1),parametr[[jseas+1]]))
       }else{
         month11=c(month11,qgamma(runif(1,0,1),parametr[[jseas+1]][1],parametr[[jseas+1]][2]))
@@ -290,7 +235,7 @@ nonprcpSim=cbind(nonprcpSim,(Z0*u_sigma0[[jseas+1]][,2]+u_sigma0[[jseas+1]][,1])
  
  
  
-nonprcpSimseason=rbind(nonprcpSimseason,nonprcpSim)
+nonprcpSimseason=rbind.data.frame(nonprcpSimseason,nonprcpSim)
 prcpSimSeason= c(prcpSimSeason,month11)
 jseas=jseas+1
 if (jseas==3){
@@ -307,8 +252,8 @@ j3=1:lmois[3]
 sim_mois_jour=cbind(rep(startyear,length(prcpSimSeason)),c(mm1,mm2,mm3),c(j1,j2,j3),round(prcpSimSeason,1))
 remove(mm1,mm2,mm3,j1,j2,j3)
 
-nonprcpSimSeasonyear=rbind(nonprcpSimSeasonyear,nonprcpSimseason)
-prcpSimSeasonyear=rbind(prcpSimSeasonyear,sim_mois_jour)
+nonprcpSimSeasonyear=rbind.data.frame(nonprcpSimSeasonyear,nonprcpSimseason)
+prcpSimSeasonyear=rbind.data.frame(prcpSimSeasonyear,sim_mois_jour)
 nbyear=nbyear+1
 startyear=startyear+1
 if (nbyear==nyear){
@@ -334,7 +279,6 @@ prcpSimSeasonyear=as.data.frame(prcpSimSeasonyear) %>%
 cum_sim_year_normal_adj=as.data.frame(prcpSimSeasonyear)%>%group_by(year)%>%summarise(rain=sum(rain),rain_adj=sum(rain_adj))
 
 nonprcpSimSeasonyear=round(nonprcpSimSeasonyear,1)
-data_sim=as.data.frame(cbind(prcpSimSeasonyear,nonprcpSimSeasonyear))
+data_sim=as.data.frame(cbind.data.frame(prcpSimSeasonyear,nonprcpSimSeasonyear))
 return(list(data_sim=data_sim,cm_seas_sim=cum_sim_year_normal_adj,parametre=para_gene_pluie))
-stopCluster(cl)
 }
